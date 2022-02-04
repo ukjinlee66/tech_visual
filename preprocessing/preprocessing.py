ca236b423286e949d042d59b53452c11b710ba4a
이용광 <sksda4614@naver.com>
2022-02-04 16:10:51 +0900

    전처리
    word count 결과 전처리 구현

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,48 @@
+import json
+import nltk
+from konlpy.tag import Okt
+import re
+# nltk.download('all')    # 최소 한번 실행해야 함
+okt = Okt()
+korean = re.compile('[\u3131-\u3163\uac00-\ud7a3]+')   # 한글 제거
+
+text_file_path = '/Users/lyk/Downloads/result.txt'    # txt 파일 위치
+
+m = {}
+
+with open(text_file_path,'r',encoding='utf-8') as f:
+  lines = f.readlines()               # 라인 별로 읽음
+  for i, l in enumerate(lines):
+    li = l.strip().split('\t')        #  \t 기준으로 split
+    li[0] = li[0].replace('\\n','')
+    cleaned = re.sub(r'[^\w\d\s]','',li[0])   # 불필요한 기호 제거
+    cleaned_content = cleaned.lower()       # 영어는 소문자로 통일
+
+    korean_noun = okt.nouns(cleaned_content)       # 한글중 명사 추출
+
+    english_only = re.sub(korean, '', cleaned_content)         # 한글 제거
+    english_word_tokens = nltk.word_tokenize(english_only)     # 영어중 단어만 추출
+    for korea in korean_noun:
+      if korea:
+        if korea.isdigit():         # 숫자는 뺌
+          continue
+        m.setdefault(korea,0)
+        m[korea]+=int(li[len(li)-1])
+    for english in english_word_tokens:
+      if english:
+        if(english.isdigit()):       # 숫자는 뺌
+          continue
+        m.setdefault(english,0)
+        m[english]+=int(li[len(li)-1])
+
+
+rev = sorted(m.items(), reverse=True, key=lambda item: item[1])
+
+m = []
+
+for word, count in rev:
+  temp={'word':word, 'count':count}
+  m.append(temp)
+
+with open("word_count.json",'w', encoding="utf-8") as outfile:
+   json.dump(m, outfile, ensure_ascii=False, indent="\t")

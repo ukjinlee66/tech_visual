1b39b4a036a31ad110e8a9eebc514e22c6b48da0
이용광 <sksda4614@naver.com>
2022-02-06 17:17:11 +0900

    preprocessing
    preprocessing 불용어 추가

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -4,17 +4,24 @@ from konlpy.tag import Okt
 import re
 # nltk.download('all')    # 최소 한번 실행해야 함
 okt = Okt()
+fname = 'total'
 korean = re.compile('[\u3131-\u3163\uac00-\ud7a3]+')   # 한글 제거
 
-text_file_path = '/Users/lyk/Downloads/result.txt'    # txt 파일 위치
+text_file_path = f'/Users/lyk/Downloads/word_count/{fname}.txt'    # txt 파일 위치
 
 m = {}
+
+stopword = ['_id','dept','company','tech','exp','qual','prefer']
 
 with open(text_file_path,'r',encoding='utf-8') as f:
   lines = f.readlines()               # 라인 별로 읽음
   for i, l in enumerate(lines):
     li = l.strip().split('\t')        #  \t 기준으로 split
     li[0] = li[0].replace('\\n','')
+    if('ObjectId' in li[0]):
+      continue
+    if li[0] in stopword:
+      continue
     cleaned = re.sub(r'[^\w\d\s]','',li[0])   # 불필요한 기호 제거
     cleaned_content = cleaned.lower()       # 영어는 소문자로 통일
 
@@ -35,14 +42,5 @@ with open(text_file_path,'r',encoding='utf-8') as f:
         m.setdefault(english,0)
         m[english]+=int(li[len(li)-1])
 
-
-rev = sorted(m.items(), reverse=True, key=lambda item: item[1])
-
-m = []
-
-for word, count in rev:
-  temp={'word':word, 'count':count}
-  m.append(temp)
-
-with open("word_count.json",'w', encoding="utf-8") as outfile:
+with open(f"/Users/lyk/Downloads/word_count to json/{fname}.json",'w', encoding="utf-8") as outfile:
    json.dump(m, outfile, ensure_ascii=False, indent="\t")
